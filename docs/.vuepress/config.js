734a881e7313a6b125597074a431bc87e827ac1d
Aldrin Martoq <a@a0.cl>
2019-09-27 10:24:03 -0300

    Se agrega GA.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,13 @@
 module.exports = {
   title: 'Bancodrone',
   description: 'API para construir robots que ejecutan operaciones en distintos bancos con un navegador, utilizando Webdrone',
-  base: '/a0-bancodrone-ruby/'
+  base: '/a0-bancodrone-ruby/',
+  plugins: [
+    [
+      '@vuepress/google-analytics',
+      {
+        'ga': 'UA-97543933-4' // UA-00000000-0
+      }
+    ]
+  ]
 }
